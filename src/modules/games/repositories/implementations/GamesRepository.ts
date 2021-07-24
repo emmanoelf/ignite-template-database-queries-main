6e3ce7fbad396ffe22ba74dad282be2aa2f23264
Emmanoel F <emmanoel_95@hotmail.com>
2021-07-24 00:18:26 -0300

    Desafio 1 - database queries

---
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -0,0 +1,30 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { User } from '../../../users/entities/User';
+import { Game } from '../../entities/Game';
+
+import { IGamesRepository } from '../IGamesRepository';
+
+export class GamesRepository implements IGamesRepository {
+  private repository: Repository<Game>;
+
+  constructor() {
+    this.repository = getRepository(Game);
+  }
+
+  async findByTitleContaining(param: string): Promise<Game[]> {
+      return this.repository.query(`SELECT * FROM games WHERE LOWER(title) like LOWER('%${param}%')`);
+  }
+
+  async countAllGames(): Promise<[{ count: string }]> {
+    return this.repository.query('SELECT COUNT(id) FROM games')
+  }
+
+  async findUsersByGameId(id: string): Promise<User[]> {
+    return this.repository
+    .createQueryBuilder()
+    .relation('users')
+    .of(id)
+    .loadMany();
+  }
+}
---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,32 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async findUserWithGamesById({
+    user_id,
+  }: IFindUserWithGamesDTO): Promise<User> {
+    return this.repository.findOneOrFail(user_id, {
+      relations: ['games'],
+    });
+  }
+
+  async findAllUsersOrderedByFirstName(): Promise<User[]> {
+    return this.repository.query('SELECT * FROM users order by first_name');
+  }
+
+  async findUserByFullName({
+    first_name,
+    last_name,
+  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+    return this.repository.query(`SELECT * FROM users WHERE LOWER(first_name)=LOWER('${first_name}') AND LOWER(last_name)=LOWER('${last_name}')`);
+  }
+}
